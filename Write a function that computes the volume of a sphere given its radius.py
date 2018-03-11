56d3798996d95e2064657b2eab871f77cb23a0cc
Rajath Bharadwaj <rajathrajath940@gmail.com>
2018-03-11 10:47:43 +0530

    Create Write a function that computes the volume of a sphere given its radius.py

---
+++ Write a function that computes the volume of a sphere given its radius.py
@@ -0,0 +1,12 @@
+Write a function that computes the volume of a sphere given its radius.
+
+#CODE
+def vol(rad):
+    pi = 3.142
+    vol = ((4.0/3))*(pi)*(rad**3)
+    return vol
+    
+
+#OUTPUT
+vol(4)
+268.1173333333333
